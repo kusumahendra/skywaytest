bc6bcae8307d4f7ac62b095c8b56e8d38e7e7a19
Kusuma Hendra <me@kusumahendra.com>
2017-12-16 01:33:53 +0800

    add some docs

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,10 +6,19 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+	/**
+	 * display blade html
+	 * @return [view]
+	 */
 	public function index(){
 		return view('index');
 	}
 
+	/**
+	 * test 1 function worker
+	 * @param  Request $request
+	 * @return json
+	 */
 	public function testOneWorker(Request $request){
 		$string = $request->sentence;
 		if ($string == '') {
@@ -24,6 +33,11 @@ class MainController extends Controller
 		]);
 	}
 
+	/**
+	 * test 2 function worker
+	 * @param  Request $request
+	 * @return json
+	 */
 	public function testTwoWorker(Request $request){
 		if ($request->limit == '') {
 			return response()->json([
@@ -43,6 +57,11 @@ class MainController extends Controller
 		]);
 	}
 
+	/**
+	 * test 2 function worker
+	 * @param  Request $request
+	 * @return json
+	 */
 	public function testThreeWorker(Request $request){
 		$numbers = $request->number;
 		$numbers = array_filter($numbers);
@@ -62,6 +81,12 @@ class MainController extends Controller
 			'average' => $average
 		]);
 	}
+
+	/**
+	 * prime number checker helper function
+	 * @param  [integer] $i
+	 * @return [boolean]
+	 */
 	public function isPrimeNumber($i) {
 	    $n = 2;
 	    while ($n < $i) {
@@ -75,9 +100,21 @@ class MainController extends Controller
 
 	    return true;
 	}
+
+	/**
+	 * find average from array number
+	 * @param  [array] $arr [array of number]
+	 * @return [integer]
+	 */
 	function average($arr){
 	    return ($arr) ? array_sum($arr)/count($arr) : 0;
 	}
+
+	/**
+	 * find median from array number
+	 * @param  [array] $arr [array of number]
+	 * @return [integer]
+	 */
 	public function median($arr){
 	    if($arr){
 	        $count = count($arr);
